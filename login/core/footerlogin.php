ca3d042440f387f0cf1b9d1be32b993a2bd5f275
JesseHardorff <144345868+JesseHardorff@users.noreply.github.com>
2024-06-05 11:19:52 +0200

    bootstrap en login dinegthes

---
+++ login/core/footerlogin.php
@@ -0,0 +1,8 @@
+
+<script src="<?=BASEURL;?>assets/js/bootstrap.bundle.min.js"></script>
+<script src="<?=BASEURL;?>assets/js/script.js"></script>
+</body>
+</html>
+<!-- <?php
+$con->close();
+?> -->
