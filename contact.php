f3c69bcf3e872183dde0b37b3c318561187e8054
TristanvanBuuren <144346103+TristanvanBuuren@users.noreply.github.com>
2024-06-05 12:26:07 +0200

    finsished contact.php

--- contact.php
+++ contact.php
@@ -1,34 +1,65 @@
 <?php
 include ('assets/core/header.php');
 ?>
-<main>
-    <form>
-        <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Uw naam</label>
-            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="John Doe">
-        </div>
+<main class="a">
+    <div class="text-contact">We streven ernaar om constant in contact te staan met onze klanten totdat de klus geklaard
+        is. Als u vragen of
+        speciale verzoeken heeft, stuur ons dan een bericht. Voor een vrijblijvende offerte kunt u contact met ons
+        opnemen
+        wanneer het u uitkomt. Wij zijn u graag van dienst!
+    </div>
+    <div class="b">
+        <form>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Uw naam</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="John Doe">
+            </div>
 
-        <div class="input-group mb-3">
-            <span class="input-group-text" id="basic-addon1">@</span>
-            <input type="email" class="form-control" placeholder="name@example.com" aria-label="Email"
-                aria-describedby="basic-addon1">
+            <div class="input-group mb-3">
+                <span class="input-group-text" id="basic-addon1">@</span>
+                <input type="email" class="form-control" placeholder="name@example.com" aria-label="Email"
+                    aria-describedby="basic-addon1">
+            </div>
+            <div class="input-group mb-3">
+                <span class="input-group-text" id="basic-addon1">+31</span>
+                <input type="text" class="form-control" placeholder="0 6 00 00 00 00" aria-label="tel"
+                    aria-describedby="basic-addon1">
+            </div>
+            <div class="col-12">
+                <label for="inputAddress" class="form-label">Address</label>
+                <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+            </div>
+            <div class="col-md-6">
+                <label for="inputCity" class="form-label">City</label>
+                <input type="text" class="form-control" id="inputCity">
+            </div>
+            <div class="col-md-2">
+                <label for="inputZip" class="form-label">postcode</label>
+                <input type="text" class="form-control" id="inputZip">
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlTextarea1" class="form-label">Vraag of opmerking</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+        </form>
+    </div>
+    <div class="c">
+        <div class="tel-nummer">
+            <div class="tel-nummer-text">Telefoon nummer:</div>
+            <div class="tel-nummer-nummer">sss</div>
         </div>
-
-        <!-- <div class="input-group mb-3">
-            <span class="input-group-text" id="basic-addon1">📞</span>
-            <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown"
-                aria-expanded="false">Dropdown</button>
-            <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-            <input type="text" class="form-control" aria-label="Text input with dropdown button">
-        </div> -->
-        <div class="input-group mb-3">
-            <span class="input-group-text" id="basic-addon1">+31</span>
-            <input type="text" class="form-control" placeholder="0 6 00 00 00 00" aria-label="tel"
-                aria-describedby="basic-addon1">
+        <div class="openings-tijden">
+            <div class="openings-tijden-text">Onze openings tijden</div>
+            <div class="openings-tijden-werk-week">Maandag - vrijdag: 07.00 - 17.00 uur</div>
+            <div class="openings-tijden-zaterdag">Zaterdag: Op afspraak</div>
+            <div class="openings-tijden-zondag">Zondag: Gesloten</div>
         </div>
-    </form>
+        <div class="links">
+            <div class="links-text">Onze sociale media</div>
+            <div class="yt">
+                <div class="yt-logo">YT LOGO</div>
+                <div class="yt-link">https://</div>
+            </div>
+        </div>
+    </div>
 </main>
