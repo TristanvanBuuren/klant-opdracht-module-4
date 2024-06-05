5502524516eb8047a3de8c41c4da26666bd11e19
TristanvanBuuren <144346103+TristanvanBuuren@users.noreply.github.com>
2024-06-05 11:25:09 +0200

    added basic stuff

---
+++ contact.php
@@ -0,0 +1,20 @@
+<<<<<<< Updated upstream
+=======
+<?php
+include ('assets/core/header.php');
+?>
+<main>
+    <form>
+        <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Uw naam</label>
+            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="John Doe">
+        </div>
+        <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Email adress</label>
+            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        </div>
+
+
+    </form>
+</main>
+>>>>>>> Stashed changes
