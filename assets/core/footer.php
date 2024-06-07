5c4ad19f3099c74c0bad8c72942ab5da7fa1f943
TristanvanBuuren <144346103+TristanvanBuuren@users.noreply.github.com>
2024-06-07 10:48:09 +0200

    made footer

--- assets/core/footer.php
+++ assets/core/footer.php
@@ -1,3 +1,12 @@
+<div class="footer">
+    <div class="footer-contact">Tel: +31 6 12 34 56 78 <br> Sms: 06 00 00 00 00 <br> Fax: ????</div>
+    <div class="footer-dagen">Onze Openings tijden: <br><br> Maandag - vrijdag: 07.00 - 17.00 uur <br> Zaterdag: Op
+        afspraak <br> Zondag: Gesloten
+    </div>
+    <div class="d"></div>
+</div>
+
 </body>
 <script></script>
+
 </html>
--- index.php
+++ index.php
@@ -1,27 +1,20 @@
 <?php
 include ('assets/core/header.php');
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Klanten Opdracht module-4.1</title>
-</head>
-<link rel="stylesheet" href="assets/css/style.css">
-<body>
+<main>
     <div class="container">
         <h1>Klanten Opdracht</h1>
         <p>hier onder komen projecten</p>
         <div class="image-placeholder">
             <?php
-                $placeholders = ['placeholder1.jpg', 'placeholder2.jpg', 'placeholder3.jpg'];
-                $randomIndex = array_rand($placeholders);
-                $selectedPlaceholder = $placeholders[$randomIndex];
+            $placeholders = ['placeholder1.jpg', 'placeholder2.jpg', 'placeholder3.jpg'];
+            $randomIndex = array_rand($placeholders);
+            $selectedPlaceholder = $placeholders[$randomIndex];
             ?>
             <img src="<?php echo $selectedPlaceholder; ?>" alt="Image Placeholder">
         </div>
     </div>
-</body>
-</html>
+</main>
+<?php
+include ('assets/core/footer.php');
+?>
