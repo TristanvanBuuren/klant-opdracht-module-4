ccd60434a2bad3299f72b820e57ff2dafa759b5d
TristanvanBuuren <144346103+TristanvanBuuren@users.noreply.github.com>
2024-06-17 10:12:27 +0200

    working on index

--- index.php
+++ index.php
@@ -2,16 +2,14 @@
 include ('assets/core/header.php');
 ?>
 <main>
-    <div class="container">
-        <h1>Klanten Opdracht</h1>
-        <p>hier onder komen projecten</p>
-        <div class="image-placeholder">
-            <?php
-            $placeholders = ['placeholder1.jpg', 'placeholder2.jpg', 'placeholder3.jpg'];
-            $randomIndex = array_rand($placeholders);
-            $selectedPlaceholder = $placeholders[$randomIndex];
-            ?>
-            <img src="<?php echo $selectedPlaceholder; ?>" alt="Image Placeholder">
+    <div class="content-4">
+        <div class="top">
+            <div class="tl"></div>
+            <div class="tr"></div>
+        </div>
+        <div class="bottom">
+            <div class="bl"></div>
+            <div class="br"></div>
         </div>
     </div>
 </main>
