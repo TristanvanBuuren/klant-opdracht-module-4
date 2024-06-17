85247c5876b65d72b6c91617ffe61cb124a691b9
TristanvanBuuren <144346103+TristanvanBuuren@users.noreply.github.com>
2024-06-17 11:19:03 +0200

    index.php

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ include ('assets/core/header.php');
 <main>
     <div class="content-4">
         <div class="top">
-            <div class="tl"></div>
-            <div class="tr"></div>
+            <div class="tl"><div class="desc">Descriptie van project</div></div>
+            <div class="tr"><img src="https://placehold.co/200x100"></div>
         </div>
         <div class="bottom">
             <div class="bl"></div>
