8b02b49d29abebc9a6c2ccf05a79898cee0797e3
TristanvanBuuren <144346103+TristanvanBuuren@users.noreply.github.com>
2024-06-19 11:07:31 +0200

    Update header.php

--- assets/core/header.php
+++ assets/core/header.php
@@ -23,17 +23,6 @@ include ('db_connect.php');
         </div>
         <div class="f">
             <button class="btn btn-primary"><a href="<?= BASEURL ?>index.php" class="button-deco">Home</a></button>
-            <?php if (isset($_SESSION['ingelogd']) && $_SESSION['ingelogd'] === true): ?>
-                <button class="btn btn-primary"><a href="account.php" class="button-deco">Account</a></button>
-                <button class="btn btn-primary"><a href="<?= BASEURL_LOGIN ?>uitloggen.php" class="button-deco">Uitloggen</a></button>
-            <?php elseif (isset($_SESSION['admin_ingelogd']) && $_SESSION['admin_ingelogd'] === true): ?>
-                <button class="btn btn-primary"><a href="<?= BASEURL_CMS ?>index.php" class="button-deco">Admin Account</a></button>
-                <button class="btn btn-primary"><a href="<?= BASEURL_LOGIN ?>uitloggen.php" class="button-deco">Uitloggen</a></button>
-            <?php else: ?>
-                <button class="btn btn-primary"><a href="login/login.php" class="button-deco">Inloggen</a></button>
-            <?php endif; ?>
-        </div>
-        <div class="g">
             <button class="btn btn-primary button-deco"><a href="<?= BASEURL ?>design.php" class="button-deco">Design</a></button>
             <button class="btn btn-primary button-deco"><a href="<?= BASEURL ?>contact.php" class="button-deco">Contact</a></button>
         </div>
