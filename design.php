54393a7a7faf71f51176ccd9a4a6dd82c212d8f2
TristanvanBuuren <144346103+TristanvanBuuren@users.noreply.github.com>
2024-06-19 10:48:37 +0200

    removed index and working on design

--- design.php
+++ design.php
@@ -1,36 +1,41 @@
 <?php
 include ('assets/core/header.php');
 ?>
-<main>
+<main class="mw-100vw">
     <div class="tuin-category tuin-ontwerp">
         <img class="icon icon-tuinontwerp" src="assets/img/icon-tuinontwerp.png" alt="icon tuinontwerp">
+        <div class="tuin-teksts">
         <div class="tuin-category-text ontwerp-category">Tuinontwerp</div>
-        <div class="tuin-text ontwerp-text">Een mooie tuin valt of staat bij een goed tuinontwerp. Als het op ontwerpen
-            aankomt,
-            maakt
-            Hendrik Hogendijk het verschil.
+            <div class="tuin-text ontwerp-text">Een mooie tuin valt of staat bij een goed tuinontwerp. Als het op ontwerpen
+                aankomt,
+                maakt
+                Hendrik Hogendijk het verschil.
+            </div>
         </div>
     </div>
     <div class="tuin-category tuin-aanleg">
         <img class="icon icon-tuinaanleg" src="assets/img/icon-tuinaanleg.png" alt="icon tuinaanleg">
+        <div class="tuin-teksts">
         <div class="tuin-category-text aanleg-category">Tuinaanleg</div>
         <div class="tuin-text aanleg-text">Het aanleggen van een tuin doen ik altijd vanuit dezelfde drive: ik wil er
             iets moois van maken.
-        </div>
+        </div></div>
     </div>
     <div class="tuin-category tuin-onderhoud">
         <img class="icon icon-tuinonderhoud" src="assets/img/icon-tuinonderhoud.png" alt="icon tuinonderhoud">
+        <div class="tuin-teksts">
         <div class="tuin-category-text onderhoud-category">Tuinonderhoud</div>
         <div class="tuin-text onderhoud-text">Een mooie tuin verdient het juiste onderhoud. Immers, goed onderhoud is
             een essentiële voorwaarde voor een
             tuin die het hele jaar door mooi blijft.
-        </div>
+        </div></div>
     </div>
     <div class="tuin-category zwemvijvers">
         <img class="icon icon-zwemvijvers" src="assets/img/icon-zwemvijvers.png" alt="icon zwemvijvers">
+        <div class="tuin-teksts">
         <div class="tuin-category-text zwemvijvers-category">zwemvijvers</div>
         <div class="tuin-text zwemvijvers-text">Zwemvijvers, is één van mjn grootste passies. En gelukkig, ze winnen aan steeds meer populariteit, waardoor ik er veel ervaring mee heb opgebouwd.
-        </div>
+        </div></div>
     </div>
 
 </main>
--- index.php
+++ index.php
@@ -2,16 +2,6 @@
 include ('assets/core/header.php');
 ?>
 <main>
-    <div class="content-4">
-        <div class="top">
-            <div class="tl"><div class="desc">Descriptie van project</div></div>
-            <div class="tr"><img src="https://placehold.co/200x100"></div>
-        </div>
-        <div class="bottom">
-            <div class="bl"></div>
-            <div class="br"></div>
-        </div>
-    </div>
 </main>
 <?php
 include ('assets/core/footer.php');
