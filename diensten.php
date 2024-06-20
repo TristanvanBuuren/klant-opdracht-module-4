652fc4a3599956fe80f0da0d0161a36c3597f694
TristanvanBuuren <144346103+TristanvanBuuren@users.noreply.github.com>
2024-06-20 11:42:20 +0200

    making responsive and mb friendly
    Not done yet.

--- diensten.php
+++ diensten.php
@@ -23,7 +23,8 @@ include ('assets/core/header.php');
         <div class="tuin-category-text aanleg-category">Tuinaanleg</div>
         <div class="tuin-text aanleg-text">Het aanleggen van een tuin doen ik altijd vanuit dezelfde drive: ik wil er
             iets moois van maken.
-        </div></div>
+        </div>
+    </div>
     </div>
     <div class="tuin-category tuin-onderhoud">
         <img class="icon icon-tuinonderhoud" src="assets/img/icon-tuinonderhoud.png" alt="icon tuinonderhoud">
@@ -32,14 +33,16 @@ include ('assets/core/header.php');
         <div class="tuin-text onderhoud-text">Een mooie tuin verdient het juiste onderhoud. Immers, goed onderhoud is
             een essentiële voorwaarde voor een
             tuin die het hele jaar door mooi blijft.
-        </div></div>
+        </div>
+    </div>
     </div>
     <div class="tuin-category zwemvijvers">
         <img class="icon icon-zwemvijvers" src="assets/img/icon-zwemvijvers.png" alt="icon zwemvijvers">
         <div class="tuin-teksts">
         <div class="tuin-category-text zwemvijvers-category">zwemvijvers</div>
         <div class="tuin-text zwemvijvers-text">Zwemvijvers, is één van mjn grootste passies. En gelukkig, ze winnen aan steeds meer populariteit, waardoor ik er veel ervaring mee heb opgebouwd.
-        </div></div>
+        </div>
+    </div>
     </div>
 
 </main>
