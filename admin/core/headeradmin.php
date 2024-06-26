e5f7ba64c54ee63934664a966e37038b8246341f
TristanvanBuuren <144346103+TristanvanBuuren@users.noreply.github.com>
2024-06-26 12:19:47 +0200

    Update headeradmin.php

--- admin/core/headeradmin.php
+++ admin/core/headeradmin.php
@@ -29,12 +29,11 @@ include ($_SERVER['DOCUMENT_ROOT'] . '/klant-opdracht-module-4/assets/core/db_co
             </div>
            
               
-                <button class="btn btn-primary">
-                    <a href="../login/uitloggen.php" class="button-deco">Uitloggen</a>
-                </button>
-           
-        </div>
-        <div><a href="<?=BASEURL_CMS;?>contactoverzicht.php" class="btn btn-primary">Contact Overzicht</a></div>
+            <div>
+                    <a href="../uitloggen.php" class="btn btn-primary">Uitloggen</a>
+                </div>
+           <div><a href="<?=BASEURL_CMS;?>contactoverzicht.php" class="btn btn-primary">Contact Overzicht</a></div>
+            <div><a class="btn btn-primary" href="<?= BASEURL_CMS ?>informatie/">Informatie pagina</a></div>
                     <div><a href="<?=BASEURL_CMS;?>index.php" class="btn btn-primary">Tuin/review pagina</a></div>
         </div>
     </div>
